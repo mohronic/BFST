22e903e95015dd3bd9e92d042af82a751636a09b
PeterOeClausen <PeterOeClausen@gmail.com>
2014-03-17 16:01:57 +0100

    Nothing

--- ProjectPart1/src/MapPack/Canvas.java
+++ ProjectPart1/src/MapPack/Canvas.java
@@ -157,6 +157,7 @@ public class Canvas extends JComponent implements MouseListener, MouseMotionList
             y1 = (-n.get(ed.FNODE - 1).Y_COORD + 6402050.98297) / 800;
             x2 = (n.get(ed.TNODE - 1).X_COORD - 442254.35659) / 800;
             y2 = (-n.get(ed.TNODE - 1).Y_COORD + 6402050.98297) / 800;
+            
             //checks whether the whole map or a dragged rectangle should be showed.            
             if (x1 * scale > mouseXStart && x2 * scale < mouseXEnd && y1 * scale > mouseYStart && y2 * scale < mouseYEnd)
             {
@@ -265,7 +266,6 @@ public class Canvas extends JComponent implements MouseListener, MouseMotionList
         {
             g.drawRect(mouseXStart, mouseYStart, currentX - mouseXStart, currentY - mouseYStart);
         }
-
         repaint();
     }
 
