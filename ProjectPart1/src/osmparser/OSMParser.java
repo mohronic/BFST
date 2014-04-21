9e04eeb61a68d8e1949cfd421b1b34a13a2237d3
z3ss <kmn1993@gmail.com>
2014-04-21 17:22:12 +0200

    Started on OSM parser

---
+++ ProjectPart1/src/osmparser/OSMParser.java
@@ -0,0 +1,25 @@
+package osmparser;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ *
+ * @author z3ss
+ */
+public class OSMParser {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        String filename = "./data/output.osm";
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(true);
+        SAXParser saxParser = spf.newSAXParser();
+        XMLReader xmlReader = saxParser.getXMLReader();
+        xmlReader.setContentHandler(new SAXHandler());
+        xmlReader.parse(SAXHandler.convertToFileURL(filename));
+    }
+}
