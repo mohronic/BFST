e2b7f66e84a5de9f3a299a8824239b2380e30039
adae <adae@itu.dk>
2014-04-22 16:21:24 +0200

    -test

--- ProjectPart1/src/FastestRoute/test.java
+++ ProjectPart1/src/FastestRoute/test.java
@@ -19,38 +19,38 @@ public class test
     //private final EdgeWeightedDigraph ewd;
     private ArrayList<DirectedEdge> list;
 
-    public test()
-    {
-        list = new ArrayList<>();
-        addEdges();
-        DijkstraSP dsp = new DijkstraSP(list, new DirectedEdge(new Point(6, 6), new Point(5, 5), 2));
-        HashMap map = dsp.getDistTo();
-        ArrayList<Linked> linked = dsp.getRoute(map, new DirectedEdge(new Point(2, 3), new Point(4, 4), 3));
-        for(Linked l : linked)
-        {
-            System.out.println(l.getFrom());
-        }
-
-        System.out.println("lel");
-
-    }
-
-    private void addEdges()
-    {
-        list.add(new DirectedEdge(new Point(6, 6), new Point(5, 5), 2));
-        list.add(new DirectedEdge(new Point(5, 5), new Point(2, 5), 1));
-        list.add(new DirectedEdge(new Point(2, 5), new Point(3, 3), 2));
-
-        list.add(new DirectedEdge(new Point(6, 6), new Point(2, 2), 7));
-        list.add(new DirectedEdge(new Point(2, 2), new Point(3, 3), 2));
-
-        list.add(new DirectedEdge(new Point(3, 3), new Point(2, 3), 6));
-        list.add(new DirectedEdge(new Point(2, 3), new Point(4, 4), 3));
-
-        list.add(new DirectedEdge(new Point(3, 3), new Point(1, 3), 5));
-
-        list.add(new DirectedEdge(new Point(3, 3), new Point(7, 10), 2));
-        list.add(new DirectedEdge(new Point(7, 10), new Point(1, 1), 5));
-        list.add(new DirectedEdge(new Point(1, 1), new Point(2, 3), 3));
-    }
+//    public test()
+//    {
+//        list = new ArrayList<>();
+//        addEdges();
+//        DijkstraSP dsp = new DijkstraSP(list, new DirectedEdge(new Point(6, 6), new Point(5, 5), 2));
+//        HashMap map = dsp.getDistTo();
+//        ArrayList<Linked> linked = dsp.getRoute(map, new DirectedEdge(new Point(2, 3), new Point(4, 4), 3));
+//        for(Linked l : linked)
+//        {
+//            System.out.println(l.getFrom());
+//        }
+//
+//        System.out.println("lel");
+//
+//    }
+//
+//    private void addEdges()
+//    {
+//        list.add(new DirectedEdge(new Point(6, 6), new Point(5, 5), 2));
+//        list.add(new DirectedEdge(new Point(5, 5), new Point(2, 5), 1));
+//        list.add(new DirectedEdge(new Point(2, 5), new Point(3, 3), 2));
+//
+//        list.add(new DirectedEdge(new Point(6, 6), new Point(2, 2), 7));
+//        list.add(new DirectedEdge(new Point(2, 2), new Point(3, 3), 2));
+//
+//        list.add(new DirectedEdge(new Point(3, 3), new Point(2, 3), 6));
+//        list.add(new DirectedEdge(new Point(2, 3), new Point(4, 4), 3));
+//
+//        list.add(new DirectedEdge(new Point(3, 3), new Point(1, 3), 5));
+//
+//        list.add(new DirectedEdge(new Point(3, 3), new Point(7, 10), 2));
+//        list.add(new DirectedEdge(new Point(7, 10), new Point(1, 1), 5));
+//        list.add(new DirectedEdge(new Point(1, 1), new Point(2, 3), 3));
+//    }
 }
