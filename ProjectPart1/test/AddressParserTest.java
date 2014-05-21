0ffe92fbd11de3aa196214baa6d5525389c6aa9e
PeterOeClausen <PeterOeClausen@gmail.com>
2014-05-21 10:22:33 +0200

    Javadoc is done Merge branch 'master' of https://github.com/z3ss/BFST
    Conflicts:
    	ProjectPart1/src/AddressSearchEngine/SearchField.java

--- ProjectPart1/test/AddressParserTest.java
+++ ProjectPart1/test/AddressParserTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,12 +37,6 @@ public class AddressParserTest
     public void tearDown()
     {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
     
     @Test
     public void A()
